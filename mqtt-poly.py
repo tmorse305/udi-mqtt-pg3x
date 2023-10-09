e6e1f1581116c46106f92c2deb22e0475da58e51
TriLife <jr@strele.ws>
2023-10-09 16:43:30 -0500

    hint back to [4,17,0,0] ver 0.0.18

--- mqtt-poly.py
+++ mqtt-poly.py
@@ -11,7 +11,7 @@ import time
 
 LOGGER = udi_interface.LOGGER
 Custom = udi_interface.Custom
-VERSION = '0.0.17'
+VERSION = '0.0.18'
 
 class Controller(udi_interface.Node):
     def __init__(self, polyglot, primary, address, name):
@@ -412,7 +412,7 @@ class MQDimmer(udi_interface.Node):
     drivers = [{'driver': 'ST', 'value': 0, 'uom': 51, 'name': 'Status'}]
 
     id = "MQDIMMER"
-    hint = [1,2,9,0]
+    hint = [4,17,0,0]
     commands = {"QUERY": query, "DON": set_on, "DOF": set_off, "BRT": brighten, "DIM": dim}
 
 
