17c4a3472c905c8dc21fac65fc1a45eabe1e6c43
tmorse305 <38409250+tmorse305@users.noreply.github.com>
2025-09-14 18:50:04 -0400

    Update MQDroplet.py

--- nodes/MQDroplet.py
+++ nodes/MQDroplet.py
@@ -72,7 +72,9 @@ class MQDroplet(udi_interface.Node):
         the parent class, so you don't need to override this method unless
         there is a need.
         """
-        self.controller.mqtt_pub(self.cmd_topic, "{"Online": 1}")
+        self.qrypayload = "{"Online": 1}"
+        LOGGER.debug(f"payload: {self.qrypayload}")
+        self.controller.mqtt_pub(self.cmd_topic, self.qrypayload)
         LOGGER.debug(f"cmd_topic: {self.cmd_topic}")
         self.reportDrivers()
         
